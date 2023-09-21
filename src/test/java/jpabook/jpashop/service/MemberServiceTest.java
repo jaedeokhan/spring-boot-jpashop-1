b9c6693fe1f486815f2812c50891eaf74f893ab2
jaedeokhan <hjaedeok@gmail.com>
2023-09-21 23:56:02 +0900

    회원 기능 테스트 * 초기 선언 차이 - @RunWith VS @ExtendWith * 예외 처리 차이 - @Test(expected = IllegalStateException.class) VS Assertions.assertThrow(exception, lambda)
    * jdbc:h2:mem:test
    
    * create는 drop and create
    * create-drop은 if exits drop and create

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+//    @Rollback(false) // 기본적으로 Spring @Transactional을 사용하면 rollback한다. 기본적으로 JPA에서는 영속성 컨텍스트 반영 X
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+//        em.flush(); // flush를 하면 DB에 쿼리가 강제로 나간다. 영속성 컨텍스트에서 DB에 쿼리를 날린다.
+        assertEquals(member, memberRepository.findMember(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+    }
+}
